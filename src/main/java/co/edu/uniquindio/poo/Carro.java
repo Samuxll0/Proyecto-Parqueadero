ecfdebed65cee7cda5708ce9a52328d0e03b1967
Samuel Ortíz Olivos <samuel.ortizo@uqvirtual.edu.co>
2024-05-16 15:19:34 -0500

    Creacion de la clase parqueadero

--- src/main/java/co/edu/uniquindio/poo/Carro.java
+++ src/main/java/co/edu/uniquindio/poo/Carro.java
@@ -1,7 +1,21 @@
 package co.edu.uniquindio.poo;
 
+/**
+ * Clase que agrupa la informacion de un carro
+ * @author Samuel Ortiz, Amed Vargas
+ * @since 2024 - 05
+ * 
+ */
 public class Carro extends Vehiculo {
 
+    /**
+     * Constructor de la clase carro
+     * 
+     * @param placa
+     * @param modelo
+     * @param propietario
+     * 
+     */
     public Carro(String placa, String modelo, Propietario propietario) {
 
         super(placa, modelo, propietario);
