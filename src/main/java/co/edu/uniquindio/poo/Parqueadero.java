7750c07206bb3f5864419aee27548d71481293a1
Samuel Ortíz Olivos <samuel.ortizo@uqvirtual.edu.co>
2024-05-16 15:45:18 -0500

    Creacion rama samuel

--- src/main/java/co/edu/uniquindio/poo/Parqueadero.java
+++ src/main/java/co/edu/uniquindio/poo/Parqueadero.java
@@ -1,9 +1,13 @@
 package co.edu.uniquindio.poo;
 
+import java.util.Collection;
 import java.util.Hashtable;
 
 public class Parqueadero {
     
+    private Collection <Vehiculo> vehiculos;
     private Hashtable <String, Puesto> puestos;
+    private Collection <Propietario> propietarios;
+    private Collection <Historial> historiales;
 
 }
