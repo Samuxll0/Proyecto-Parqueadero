24908d5200e22e58674fdfa3b14cb0f876245a07
Samuel Ortíz Olivos <samuel.ortizo@uqvirtual.edu.co>
2024-05-22 16:55:56 -0500

    Cambios

--- src/main/java/co/edu/uniquindio/poo/Historial.java
+++ src/main/java/co/edu/uniquindio/poo/Historial.java
@@ -2,20 +2,59 @@ package co.edu.uniquindio.poo;
 
 import java.time.LocalDateTime;
 
+/**
+ * Clase que agrupa la información de el historial de varios vehiculos.
+ * 
+ * @author Samuel Ortiz, Amed Vargas
+ * @since 2024 - 05
+ * 
+ */
 public class Historial {
-
     private LocalDateTime fechaEntrada;
+    private final Vehiculo vehiculo;
+    private final Puesto puesto;
 
-    public Historial(LocalDateTime fechaEntrada) {
+    /**
+     * Metodo constructor de la clase historial.
+     * 
+     * @param fechaEntrada
+     */
+    public Historial(LocalDateTime fechaEntrada, Vehiculo vehiculo, Puesto puesto) {
+        
         this.fechaEntrada = fechaEntrada;
-    }
+        this.vehiculo = vehiculo;
+        this.puesto = puesto;
+    }   
 
+    /**
+     * Metodo que obtiene la fecha de entrada de un vehiculo.
+     * 
+     * @return
+     */
     public LocalDateTime getFechaEntrada() {
         return fechaEntrada;
     }
 
-    public void fechaSalida() {
+    /**
+     * Metodo para obtener un vehiculo
+     * 
+     * @return
+     */
+    public Vehiculo getVehiculo() {
+        return vehiculo;
+    }
 
+    /**
+     * Metodo para obtener un puesto
+     * 
+     * @return
+     */
+    public Puesto getPuesto() {
+        return puesto;
+    }
+
+    public void calcularCosto(LocalDateTime fechaEntrada, Puesto puesto) {
+        
     }
 
 }
--- src/main/java/co/edu/uniquindio/poo/Parqueadero.java
+++ src/main/java/co/edu/uniquindio/poo/Parqueadero.java
@@ -1,13 +1,63 @@
 package co.edu.uniquindio.poo;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Hashtable;
+import java.util.LinkedList;
 
+/**
+ * Clase que agrupa la informacion de un parqueadero.
+ * 
+ * @author Samuel Ortiz, Amed Vargas
+ * @since 2024 - 05
+ * 
+ */
 public class Parqueadero {
+    private final int maximoFilas;
+    private final int maximoColumnas;
     
     private Collection <Vehiculo> vehiculos;
     private Hashtable <String, Puesto> puestos;
     private Collection <Propietario> propietarios;
     private Collection <Historial> historiales;
+
+    /**
+     * Metodo constructor de la clase parqueadero
+     */
+    public Parqueadero(int maximoFilas, int maximoColumnas){
+        assert maximoFilas > 0: "El maximo de filas debe ser positivo";
+        assert maximoColumnas > 0: "El maximo de columnas debe ser positivo";
+
+        this.maximoFilas = maximoFilas;
+        this.maximoColumnas = maximoColumnas;
+
+        vehiculos = new LinkedList<>();
+        puestos = new Hashtable<>();
+        propietarios = new LinkedList<>();
+        historiales = new LinkedList<>();
+    }
+
+    /**
+     * Metodo para obtener un vehiculo de una coleccion de vehiculos
+     * 
+     * @return
+     */
+    public Collection<Vehiculo> getVehiculos(){
+        return Collections.unmodifiableCollection(vehiculos);
+    }
+
+    /**
+     * Metodo para obtener un propietario del parqueadero.
+     * 
+     * @return
+     */
+    public Collection<Propietario> getPropietarios(){
+        return Collections.unmodifiableCollection(propietarios);
+    }
+
+    public Collection<Historial> getHistoriales(){
+        return Collections.unmodifiableCollection(historiales);
+    }
+
 
 }
